5cbff3a660ac9e2714f74bbd66b4253d76125f24
PassionPI <50688829+PassionPI@users.noreply.github.com>
2024-09-26 11:45:22 +0800

    Update reselectBy.ts

--- src/util/reselectBy.ts
+++ src/util/reselectBy.ts
@@ -1,26 +1,22 @@
-export function reselectBy<T>(getState: () => T) {
-  const reselectByState = <S extends Readonly<((input: T) => unknown)[]>, R>(
-    selectors: S,
-    calc: (...args: { [Key in keyof S]: ReturnType<S[Key]> }) => R
-  ) => {
-    type Args = { [Key in keyof S]: ReturnType<S[Key]> };
-    const last: { args: Args; result: R } = {
-      args: [] as Args,
-      result: null as R,
-    };
-
-    const getSnapshot = (): R => {
-      const snapshot = getState();
-      const args = selectors.map((selector) => selector(snapshot)) as Args;
-      if (args.some((arg, index) => !Object.is(arg, last.args[index]))) {
-        last.args = args;
-        last.result = calc(...args);
-      }
-      return last.result;
-    };
-
-    return getSnapshot;
+export const reselect = <T, S extends Readonly<((input: T) => unknown)[]>, R>(
+  getter: () => T,
+  selectors: S,
+  calc: (...args: { [Key in keyof S]: ReturnType<S[Key]> }) => R,
+) => {
+  type Args = { [Key in keyof S]: ReturnType<S[Key]> };
+  const last: { args: Args; result: R } = {
+    args: [] as Args,
+    result: null as R,
   };
 
-  return reselectByState;
-}
+  const getSnapshot = (): R => {
+    const args = selectors.map((selector) => selector(getter())) as Args;
+    if (args.some((arg, index) => !Object.is(arg, last.args[index]))) {
+      last.args = args;
+      last.result = calc(...args);
+    }
+    return last.result;
+  };
+
+  return getSnapshot;
+};
